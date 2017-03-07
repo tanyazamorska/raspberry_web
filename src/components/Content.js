b6f8271cd4bcf240056f5ab337635c749bf254af
Tanya <zamorskatetiana@gmail.com>
2017-03-08 01:00:10 +0200

    added folders

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,8 +3,22 @@ import React from 'react';
 export default class Footer extends React.Component {
   render() {
     return (
-    <div className="body">
-    </div>
+      <div className="container-fluid text-center">
+        <div className="row content">
+          <div className="col-sm-3">
+            <a href="#" className="btn btn-primary folder">file manager</a>
+          </div>
+          <div className="col-sm-3">
+          <a href="#" className="btn btn-warning folder"></a>
+          </div>
+          <div className="col-sm-3">
+            <a href="#" className="btn btn-info folder"></a>
+          </div>
+          <div className="col-sm-3">
+            <a href="#" className="btn btn-danger folder"></a>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,10 @@ import React from 'react';
 export default class Footer extends React.Component {
   render() {
     return (
-      <div className="footer">
-        Footer
-      </div>
+      <footer className="container-fluid text-center">
+        <hr className="main-hr" />
+          <p className="footer-copyright">© Company,&nbsp;2017. Усі права захищено.</p>
+      </footer>
     )
   }
 }
