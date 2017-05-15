7726abc33c242171b6e3c58fff07d3b72e9ca86c
Tanya <zamorskatetiana@gmail.com>
2017-05-15 22:25:42 +0300

    remove all function from Folder.render

--- src/pages/file-manager/Folder.js
+++ src/pages/file-manager/Folder.js
@@ -10,6 +10,96 @@ import MyTheme from '../../MyTheme';
 import variables from '../../variables';
 
 export class Folder extends React.Component {
+  setSizeOfFile = (size, kind) => {
+    if (kind === `file`) {
+      if (size < 1000) {
+        size = size + ` B`;
+      } else if (size > 1000 && size < 99999) {
+        size = (size / 1000).toFixed(1) + ` kB`;
+      } else if (size < 1000000000) {
+        size = (size / 1000000).toFixed(1) + ` MB`;
+      }
+    } else {
+      size = `-`;
+    }
+    return size;
+  };
+
+  dateModified = (modified) => {
+    const date = moment(modified);
+    const day = date.format(`DD`); // 23
+    const year = date.format(`YYYY`); // 2015
+    let lastModified = null; // 'YYYY.MM.DD hh:mm a'
+
+    if (moment().format(`DD`) === day) {
+      lastModified = date.format(`hh:mm a`);
+      if (this.props.name === `..`) {
+        lastModified = `-`;
+      }
+    } else if (moment().format(`YYYY`) === year) {
+      lastModified = date.format(`MMMM DD`);
+    } else {
+      lastModified = date.format(`YYYY.MM.DD`);
+    }
+    return lastModified;
+  };
+
+  url = (pathToGo, name) => {
+    // to kill double slash
+    pathToGo = (pathToGo === `/`) ? `` : pathToGo;
+    // ex: "asd/asd/asd" => ["asd", "asd", "asd"]
+    const arr = pathToGo.split(`/`);
+    if (name === `..`) {
+      arr.pop();
+      pathToGo = arr.join(`/`) + `/`;
+    } else {
+      pathToGo = pathToGo + `/` + name + `/`;
+    }
+    return pathToGo;
+  };
+
+  editorIcon = (file, size) => {
+    const linkToEditor = (
+      <Link to={`/editor${this.props.path}/${this.props.name}`}
+            title='editor'
+            target='_blank'><EditorModeEdit />
+      </Link>
+    );
+
+    if (file === `file` && size < 100000) {
+      const nameOfFiles = this.props.name;
+      const positionDot = nameOfFiles.lastIndexOf(`.`);
+      const fileExtension = nameOfFiles.slice(positionDot + 1);
+      if (nameOfFiles.charAt(0) === `.`) {
+        return linkToEditor;
+      } else if (positionDot === -1) {
+        return linkToEditor;
+      } else if (positionDot !== -1) {
+        const arrayFilesExtension = [`txt`, `md`, `ahk`, `applescript`, `as`, `au3`, `bat`, `bas`, `cljs`, `cmd`,
+          `coffee`, `duino`, `egg`, `egt`, `erb`, `hta`, `ibi`, `ici`, `ijs`, `ipynb`, `itcl`, `js`, `jsfl`, `lua`,
+          `m`, `mrc`, `ncf`, `nuc`, `nud`, `nut`, `php`, `pl`, `pm`, `ps1`, `ps1xml`, `psc1`, `psd1`, `psm1`, `py`,
+          `pyc`, `pyo`, `r`, `rb`, `rdp`, `scpt`, `scptd`, `sdl`, `sh`, `syjs`, `sypy`, `tcl`, `vbs`, `xpl`, `ebuild`,
+          `ada`, `adb`, `ads`, `asm`, `s`, `bas`, `bb`, ` bmx`, `c`, `clj`, `cls`, `cob`, `cbl`, `cpp`, `cc`, `cxx`,
+          `c`, `cbp`, `cs`, `csproj`, `d`, `dba`, `bpro123`, `e`, ` efs`, `egt`, `el`, `for`, `ftn`, `f`, `f77`,
+          `f90`, `frm`, `frx`, `fth`, `ged`, `gm6`, `gmd`, `gmk`, `gml`, `h`, `hpp`, `hxx`, ` hs`, `i`, `inc`, `java`,
+          `l`, `lgt`, `lisp`, `m4`, `ml`, `msqr`, `n`, `nb`, `p`, `pas`, `pp`, `php3`, `php4`, `php5`, `phps`, `phtml`,
+          `pisrc`, `piv`, `pli`, `pl1`, `prg`, `pro`, `pol`, `py`, `red`, `reds`, `resx`, `rc`, `rc2`, `rkt`, `rktl`,
+          `scala`, `sci`, `sce`, `scm`, `sd7`, `skb`, `skc`, `skd`, `skf`, `skg`, `ski`, `skk`, `skm`, `sko`, `skp`,
+          `skq`, `sks`, `skt`, `skz`, `sln`, `spin`, `stk`, `swg`, `tcl`, `vap`, `vb`, `vbg`, `vbp`, `vip`, `vbproj`,
+          `vcproj`, `vdproj`, `xpl`, `xq`, `xsl`, `y`, `css`, `html`, `sass`, `mysql`, `json`, `handlebars`, `go`];
+        if (arrayFilesExtension.indexOf(fileExtension) !== -1) {
+          return linkToEditor;
+        }
+      }
+    }
+  };
+
+  downloadIcon = (file) => {
+    if (file === `file`) {
+      return <FileFileDownload style={{marginRight: `20px`}}/>;
+    }
+  };
+
   render() {
     let additionalClass = null;
     if (this.props.kind === `folder`) {
@@ -20,101 +110,11 @@ export class Folder extends React.Component {
         style={{height: `44px`, width: `44px`, marginRight: `20px`, color: MyTheme.palette.borderColor}}/>;
     }
 
-    const dateModified = (modified) => {
-      const date = moment(modified);
-      const day = date.format(`DD`); // 23
-      const year = date.format(`YYYY`); // 2015
-      let lastModified = null; // 'YYYY.MM.DD hh:mm a'
-
-      if (moment().format(`DD`) === day) {
-        lastModified = date.format(`hh:mm a`);
-        if (this.props.name === `..`) {
-          lastModified = `-`;
-        }
-      } else if (moment().format(`YYYY`) === year) {
-        lastModified = date.format(`MMMM DD`);
-      } else {
-        lastModified = date.format(`YYYY.MM.DD`);
-      }
-      return lastModified;
-    };
-
-    const setSizeOfFile = (size, kind) => {
-      if (kind === `file`) {
-        if (size < 1000) {
-          size = size + ` B`;
-        } else if (size > 1000 && size < 99999) {
-          size = (size / 1000).toFixed(1) + ` kB`;
-        } else if (size < 1000000000) {
-          size = (size / 1000000).toFixed(1) + ` MB`;
-        }
-      } else {
-        size = `-`;
-      }
-      return size;
-    };
-
-    const url = (pathToGo, name) => {
-      // to kill double slash
-      pathToGo = (pathToGo === `/`) ? `` : pathToGo;
-      // ex: "asd/asd/asd" => ["asd", "asd", "asd"]
-      const arr = pathToGo.split(`/`);
-      if (name === `..`) {
-        arr.pop();
-        pathToGo = arr.join(`/`) + `/`;
-      } else {
-        pathToGo = pathToGo + `/` + name + `/`;
-      }
-      return pathToGo;
-    };
-
-    const editorIcon = (file, size) => {
-      const linkToEditor = (
-        <Link to={`/editor${this.props.path}/${this.props.name}`}
-              title='editor'
-              target='_blank'><EditorModeEdit />
-        </Link>
-      );
-
-      if (file === `file` && size < 100000) {
-        const nameOfFiles = this.props.name;
-        const positionDot = nameOfFiles.lastIndexOf(`.`);
-        const fileExtension = nameOfFiles.slice(positionDot + 1);
-        if (nameOfFiles.charAt(0) === `.`) {
-          return linkToEditor;
-        } else if (positionDot === -1) {
-          return linkToEditor;
-        } else if (positionDot !== -1) {
-          const arrayFilesExtension = [`txt`, `md`,  `ahk`,  `applescript`, `as`, `au3`, `bat`, `bas`, `cljs`,`cmd`,
-            `coffee`, `duino`, `egg`, `egt`, `erb`, `hta`, `ibi`, `ici`, `ijs`, `ipynb`, `itcl`, `js`, `jsfl`, `lua`,
-            `m`, `mrc`, `ncf`, `nuc`, `nud`, `nut`, `php`, `pl`, `pm`, `ps1`, `ps1xml`, `psc1`, `psd1`, `psm1`, `py`,
-            `pyc`, `pyo`, `r`, `rb`, `rdp`, `scpt`, `scptd`, `sdl`, `sh`, `syjs`, `sypy`, `tcl`, `vbs`, `xpl`, `ebuild`,
-            `ada`, `adb`, `ads`, `asm`, `s`, `bas`, `bb`, ` bmx`, `c`, `clj`, `cls`, `cob`, `cbl`, `cpp`, `cc`, `cxx`,
-            `c`, `cbp`, `cs`, `csproj`, `d`, `dba`, `bpro123`, `e`, ` efs`, `egt`, `el`, `for`, `ftn`, `f`, `f77`,
-            `f90`, `frm`, `frx`, `fth`, `ged`, `gm6`, `gmd`, `gmk`, `gml`, `h`, `hpp`, `hxx`, ` hs`, `i`, `inc`, `java`,
-            `l`, `lgt`, `lisp`, `m4`, `ml`, `msqr`, `n`, `nb`, `p`, `pas`, `pp`, `php3`, `php4`, `php5`, `phps`, `phtml`,
-            `pisrc`, `piv`, `pli`, `pl1`, `prg`, `pro`, `pol`, `py`, `red`, `reds`, `resx`, `rc`, `rc2`, `rkt`, `rktl`,
-            `scala`, `sci`, `sce`, `scm`, `sd7`, `skb`, `skc`, `skd`, `skf`, `skg`, `ski`, `skk`, `skm`, `sko`, `skp`,
-            `skq`, `sks`, `skt`, `skz`, `sln`, `spin`, `stk`, `swg`, `tcl`, `vap`, `vb`, `vbg`, `vbp`, `vip`, `vbproj`,
-            `vcproj`, `vdproj`, `xpl`, `xq`, `xsl`, `y`, `css`, `html`, `sass`, `mysql`, `json`, `handlebars`, `go`];
-          if (arrayFilesExtension.indexOf(fileExtension) !== -1) {
-            return linkToEditor;
-          }
-        }
-      }
-    };
-
-    const downloadIcon = (file) => {
-      if (file === `file`) {
-        return <FileFileDownload style={{marginRight: `20px`}}/>;
-      }
-    };
-
     const linkToGo = (file) => {
       if (file === `folder`) {
         return (
           <Link
-            to={`/file-manager/` + this.props.hideHidden + `/` + this.props.sortBy + url(this.props.path, this.props.name)}
+            to={`/file-manager/` + this.props.hideHidden + `/` + this.props.sortBy + this.url(this.props.path, this.props.name)}
             title='open'>
             <span>{additionalClass}</span>
             <span style={{position: `relative`, top: `-15px`}}>{this.props.name}</span>
@@ -139,19 +139,19 @@ export class Folder extends React.Component {
 
         </TableRowColumn>
         <TableRowColumn>
-          {setSizeOfFile(this.props.size, this.props.kind)}
+          {this.setSizeOfFile(this.props.size, this.props.kind)}
         </TableRowColumn>
         <TableRowColumn>
-          {dateModified(this.props.lastModified)}
+          {this.dateModified(this.props.lastModified)}
         </TableRowColumn>
         <TableRowColumn>
           {
             this.props.kind === `file` ? <a href={`${variables.url}download${this.props.path}/${this.props.name}`}
-                                            title='download' target='_blank'>{downloadIcon(this.props.kind)}
+                                            title='download' target='_blank'>{this.downloadIcon(this.props.kind)}
             </a> : null
           }
           {
-            editorIcon(this.props.kind, this.props.size)
+            this.editorIcon(this.props.kind, this.props.size)
           }
         </TableRowColumn>
       </TableRow>
