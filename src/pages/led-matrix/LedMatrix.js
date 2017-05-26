6f5d0096adad33e8fd81125e8bc6da85c8d08338
Tanya <zamorskatetiana@gmail.com>
2017-05-26 15:39:36 +0300

    fix in ledMatrix tabs

--- src/pages/led-matrix/LedMatrix.js
+++ src/pages/led-matrix/LedMatrix.js
@@ -3,13 +3,30 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 import scssVariables from '../../scssVariables';
 
 export default class LedMatrix extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.location.pathname,
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({value});
+  };
+
+  handleActive = (tab) => {
+    this.props.router.push(tab.props.value);
+  };
+
   render() {
     return (
       <div style={{width: scssVariables.width}}>
-        <Tabs style={{marginBottom: `70px`}}>
-          <Tab label='Manual' onActive={() => this.props.router.push(`/led-matrix/manual/`)}>
+        <Tabs value={this.state.value}
+              onChange={this.handleChange}
+              style={{marginBottom: `70px`}}>
+          <Tab label='Manual' value='/led-matrix/manual/' onActive={this.handleActive}>
           </Tab>
-          <Tab label='Ticker' onActive={() => this.props.router.push(`/led-matrix/ticker/`)}>
+          <Tab label='Ticker' value='/led-matrix/ticker/' onActive={this.handleActive}>
           </Tab>
         </Tabs>
         {this.props.children}
